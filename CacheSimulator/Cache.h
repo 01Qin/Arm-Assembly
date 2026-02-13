5f3360117ed648260313295ccf34b430f3c314bb
01Qin <sisukasqin@hotmail.com>
2026-02-13 20:51:11 +0200

    Add accessors and a getAccesses() counter

--- CacheSimulator/Cache.h
+++ CacheSimulator/Cache.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <regex>
 #include <vector>
 #include "Memory.h"
 #include "CacheSet.h"
@@ -9,15 +10,34 @@ class Cache :
 public:
 	Cache(int accessTime, int size, int blockSize, int associativity, Memory *lowerLevel);
 	~Cache(void);
-	int read(uint64_t address);
-	int write(uint64_t address);
+
+	int read(uint64_t address) override;
+	int write(uint64_t address) override;
 	int getMisses(void);
 	int getHits(void);
+
+	int getAccess(void) const { return hits + misses; }
+
+	double getMissRate (void) const {
+		int a = getAccess();
+		return a ? static_cast<double> (misses) / static_cast<double> (a) : 0.0;
+	}
+
+	double getHitRate (void) const {
+		int a = getAccess();
+		return a ? static_cast<double>(hits) / static_cast<double>(a) : 0.0;
+	}
+
+	int getBlockSize() const { return blockSize; }
+	int getAssociativity() const { return associativity; }
+	size_t getNumSets() const { return sets.size();}
+	Memory* getLower() const { return lower; }
 private:
 	Memory *lower;
 	std::vector<CacheSet *> sets;
-	int blockSize;
-	int hits;
-	int misses;
+	int blockSize{};
+	int associativity{};
+	int hits{};
+	int misses{};
 };
 
